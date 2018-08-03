7482ad176b8b6f11ebf73c5f4351c4612fa3f651
blacksandbox <sandcubed@gmail.com>
2018-08-02 23:29:00 -0400

    Installed dependencies on the skeleton site generated by express-generater

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,9 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+
+  //TODO: learn about what "next" does. It allows you to
+  //	  can used to add multiple route handlers the path
 });
 
 module.exports = router;
