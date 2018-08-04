0b7804414c3fe44939fbbd71f60df44737da138c
blacksandbox <sandcubed@gmail.com>
2018-08-04 12:14:06 -0400

    Populate index with rough drafts of project informations

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,18 @@ router.get('/', function(req, res, next) {
   	});
 });
 
+// Example with a controller
+/*
+// Require controller modules.
+var book_controller = require('../controllers/bookController');
+
+// GET catalog home page.
+router.get('/', book_controller.index);
+// GET request for creating a Book. NOTE This must come before routes that display Book (uses id).
+router.get('/book/create', book_controller.book_create_get);
+router.post('/book/create', book_controller.book_create_post);
+router.get('/book/:id/delete', book_controller.book_delete_get);
+router.post('/book/:id/delete', book_controller.book_delete_post);
+*/
+
 module.exports = router;
